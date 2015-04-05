60f4d149555ef4f49d8559fb6727b76f8be1b3ca
Tommaso Barbugli <tbarbugli@gmail.com>
2015-04-05 18:27:57 -0400

    better tests

--- test/backends/mongoose.mocha.js
+++ test/backends/mongoose.mocha.js
@@ -11,36 +11,183 @@ var userSchema = Schema({
   name    : String
 });
 
+var linkSchema = Schema({
+  href    : String
+});
+
 var tweetSchema = Schema({
   text    : String,
-  actor   : { type: Schema.Types.ObjectId, ref: 'User' }
+  actor   : { type: Schema.Types.ObjectId, ref: 'User' },
+  bg      : String,
+  link    : { type: Schema.Types.ObjectId, ref: 'Link' }
 });
 
 StreamMongoose.activitySchema(tweetSchema);
 
-tweetSchema.statics.pathsToPopulate = function(){
-  return 'actor';
+tweetSchema.statics.pathsToPopulate = function() {
+  return ['actor', 'link'];
 };
 
-tweetSchema.methods.activityActorProp = function(){
+tweetSchema.methods.activityExtraData = function() {
+  return {'bg': this.bg, 'link': this.link};
+}
+
+tweetSchema.methods.activityActorProp = function() {
   return 'actor';
 }
 
 var Tweet = mongoose.model('Tweet', tweetSchema);
-StreamMongoose.activityModel(Tweet);
 
 var User = mongoose.model('User', userSchema);
+var Link = mongoose.model('Link', linkSchema);
+var backend = new StreamMongoose.Backend();
 
-describe('Enricher', function() {
+describe('Backend', function() {
 
     before(function(done) {
       actor = new User({'name': 'actor1'});
       actor.save();
       this.actor = actor;
+      link = new Link({'href': 'https://getstream.io'});
+      link.save();
+      this.link = link;
       done();
     });
 
-    it('serialise objects into refs', function() {
+    it('enrich aggregated activity complex mix', function(done) {
+        var self = this;
+        var tweet = new Tweet();
+        tweet.text = 'test';
+        tweet.actor = this.actor;
+        tweet.save(function(err) {
+            var activity = tweet.createActivity();
+            backend.serializeActivities([activity]);
+            var aggregatedActivities = [
+              {'actor_count': 1, 'activities': [activity]},
+            ];
+            backend.enrichAggregatedActivities(aggregatedActivities, function(err, enriched){
+              enriched.should.length(1);
+              enriched[0].should.have.property('activities').with.lengthOf(1);
+              enriched[0]['activities'][0].should.have.property('actor');
+              enriched[0]['activities'][0].should.have.property('object');
+              enriched[0]['activities'][0].should.have.property('verb');
+              done();
+            });
+        });
+    });
+
+    it('enrich aggregated activity', function(done) {
+        var self = this;
+        var tweet = new Tweet();
+        tweet.text = 'test';
+        tweet.actor = this.actor;
+        tweet.save(function(err) {
+            var activity = tweet.createActivity();
+            backend.serializeActivities([activity]);
+            var aggregatedActivities = [
+              {'actor_count': 1, 'activities': [activity]},
+            ];
+            backend.enrichAggregatedActivities(aggregatedActivities, function(err, enriched){
+              enriched.should.length(1);
+              enriched[0].should.have.property('activities').with.lengthOf(1);
+              enriched[0]['activities'][0].should.have.property('actor');
+              enriched[0]['activities'][0].should.have.property('object');
+              enriched[0]['activities'][0].should.have.property('verb');
+              done();
+            });
+        });
+    });
+
+    it('enrich aggregated activity with 2 groups', function(done) {
+        var self = this;
+        var tweet = new Tweet();
+        tweet.text = 'test';
+        tweet.actor = this.actor;
+        tweet.save(function(err) {
+            var activity = tweet.createActivity();
+            backend.serializeActivities([activity]);
+            var aggregatedActivities = [
+              {'actor_count': 1, 'activities': [activity]},
+              {'actor_count': 1, 'activities': [activity, activity]},
+            ];
+            backend.enrichAggregatedActivities(aggregatedActivities, function(err, enriched){
+              enriched.should.length(2);
+              enriched[0].should.have.property('activities').with.lengthOf(1);
+              enriched[0]['activities'][0].should.have.property('actor');
+              enriched[0]['activities'][0].should.have.property('object');
+              enriched[0]['activities'][0].should.have.property('verb');
+
+              enriched[1].should.have.property('activities').with.lengthOf(2);
+              enriched[1]['activities'][0].should.have.property('actor');
+              enriched[1]['activities'][0].should.have.property('object');
+              enriched[1]['activities'][0].should.have.property('verb');
+              done();
+            });
+        });
+    });
+
+    it('enrich one activity', function(done) {
+        var self = this;
+        var tweet = new Tweet();
+        tweet.text = 'test';
+        tweet.actor = this.actor;
+        tweet.save(function(err) {
+            var activity = tweet.createActivity();
+            backend.enrichActivities([activity], function(err, enriched){
+              enriched.should.length(1);
+              enriched[0].should.have.property('actor');
+              enriched[0]['actor'].should.have.property('_id', self.actor._id);
+              enriched[0].should.have.property('object');
+              enriched[0]['object'].should.have.property('_id', tweet._id);
+              enriched[0]['object'].should.have.property('text', tweet.text);
+              done();
+            });
+        });
+    });
+
+    it('custom fields enrichment', function(done) {
+        var self = this;
+        var tweet = new Tweet();
+        tweet.text = 'test';
+        tweet.bg = 'bgvalue';
+        tweet.actor = this.actor;
+        tweet.link = this.link;
+        tweet.save(function(err) {
+            var activity = tweet.createActivity();
+            backend.enrichActivities([activity], function(err, enriched){
+              enriched.should.length(1);
+              enriched[0].should.have.property('actor');
+              enriched[0]['actor'].should.have.property('_id', self.actor._id);
+              enriched[0].should.have.property('object');
+              enriched[0]['object'].should.have.property('_id', tweet._id);
+              enriched[0]['object'].should.have.property('text', tweet.text);
+              enriched[0].should.have.property('bg', 'bgvalue');
+              enriched[0].should.have.property('link');
+              enriched[0]['link'].should.have.property('_id', this.link._id);
+              done();
+            });
+        });
+    });
+
+    it('custom fields serialisation', function(done) {
+        var self = this;
+        var tweet = new Tweet();
+        tweet.text = 'test';
+        tweet.bg = 'bgvalue';
+        tweet.actor = this.actor;
+        tweet.link = this.link;
+        tweet.save(function(err) {
+            var activity = tweet.createActivity();
+            tweet.getStreamBackend().serializeActivities([activity]);
+            (activity).should.have.property('actor', 'User:' + tweet.actor._id);
+            (activity).should.have.property('link', 'Link:' + tweet.link._id);
+            (activity).should.have.property('bg', 'bgvalue');
+            (activity).should.have.property('object', 'Tweet:' + tweet._id);
+            done();
+        });
+    });
+
+    it('serialise objects into refs', function(done) {
         var self = this;
         var tweet = new Tweet();
         tweet.text = 'test';
@@ -50,30 +197,30 @@ describe('Enricher', function() {
             tweet.getStreamBackend().serializeActivities([activity]);
             (activity).should.have.property('actor', 'User:' + tweet.actor._id);
             (activity).should.have.property('object', 'Tweet:' + tweet._id);
+            done();
         });
     });
 
-    it('enrich one activity', function() {
+    it('enrich one activity', function(done) {
         var self = this;
         var tweet = new Tweet();
         tweet.text = 'test';
         tweet.actor = this.actor;
         tweet.save(function(err) {
             var activity = tweet.createActivity();
-            var enricher = new StreamMongoose.Backend();
-            enricher.enrichActivities([activity], function(err, enriched){
+            backend.enrichActivities([activity], function(err, enriched){
               enriched.should.length(1);
               enriched[0].should.have.property('actor');
               enriched[0]['actor'].should.have.property('_id', self.actor._id);
               enriched[0].should.have.property('object');
               enriched[0]['object'].should.have.property('_id', tweet._id);
               enriched[0]['object'].should.have.property('text', tweet.text);
+              done();
             });
         });
     });
 
-    it('enrich two activity', function() {
-        var enricher = new StreamMongoose.Backend();
+    it('enrich two activity', function(done) {
         var tweet1 = new Tweet();
         tweet1.text = 'test1';
         actor = new User({'name': 'actor1'});
@@ -84,17 +231,18 @@ describe('Enricher', function() {
         tweet2.actor = actor2._id;
 
         async.each([tweet1, tweet2], 
-          function(obj, done){
-            obj.save(function(err) { done()})
+          function(obj, cb){
+            obj.save(function(err) { cb()})
           },
           function(){
             var activities = [tweet1.createActivity(), tweet2.createActivity()];
-            enricher.enrichActivities(activities,
+            backend.enrichActivities(activities,
               function(err, enriched){
                 enriched.should.length(2);
                 enriched[0].should.have.property('foreign_id');
                 enriched[1].should.have.property('foreign_id');
                 enriched[0]['foreign_id'].should.not.equal(enriched[1]['foreign_id']);
+                done();
               }
             )}
           );
