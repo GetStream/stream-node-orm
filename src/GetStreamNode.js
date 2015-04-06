b800a1142b6d4d7b6771339013d3f342282e63ff
Mario Delgado <mariodel@gmail.com>
2015-04-06 17:12:00 -0600

    module exports StreamMongoose

--- src/GetStreamNode.js
+++ src/GetStreamNode.js
@@ -2,6 +2,7 @@ var FeedManager = require('./FeedManager.js');
 var config = require('./config.js');
 var settings = config();
 var BaseBackend = require('./backends/base.js');
+var StreamMongoose = require('./backends/mongoose.js');
 
 // TODO: make sure this happens only once!
 module.exports.FeedManager = new FeedManager(settings);
