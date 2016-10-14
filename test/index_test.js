9e443ff61c1fb6e5dd09d300c30de0eb04cd48b6
Matthisk Heimensen <m@tthisk.nl>
2016-10-14 16:27:12 +0200

    Improve test coverage

--- test/index_test.js
+++ test/index_test.js
@@ -1,5 +1,11 @@
-var should = require('should');
-var BaseBackend = require('../src/backends/base.js');
+var should = require('should')
+  , expect = require('expect.js')
+  , fs = require('fs')
+  , path = require('path')
+  , Config = require('../src/config')
+  , BaseBackend = require('../src/backends/base.js')
+  , FeedManager = require('../src/FeedManager')
+  , main = require('../src/index');
 
 describe('StreamBackend', function () {
 
@@ -21,4 +27,71 @@ describe('StreamBackend', function () {
     it('#enrichAggregatedActivities()', function () {
     });
 
+});
+
+describe('feedManagerFactory', function() {
+
+    it('should return a FeedManager instance', function() {
+        var fm = main.feedManagerFactory({});
+
+        expect(fm).to.be.a(FeedManager);
+    });
+
+    it('should ignore unkown settings', function() {
+        var fm = main.feedManagerFactory({
+            unknown: 'abcdefg'
+        });
+
+        expect(fm).to.be.a(FeedManager);
+        expect(fm.settings.unknown).to.be.undefined;
+    });
+
+    it('should override known settings', function() {
+        var fm = main.feedManagerFactory({
+            apiKey: 'abcdefg'
+        });
+
+        expect(fm).to.be.a(FeedManager);
+        expect(fm.settings.apiKey).to.be('abcdefg');
+    });
+
+});
+
+describe('Feed manager', function() {
+
+    it('default config', function() {
+        var settings = main.FeedManager.settings;
+
+        var expected = require('../getstream.js').config;
+        expect(settings).to.eql(expected);
+    });
+
+    describe('haha', function() {
+        var configDir = path.join(__dirname, './tmp');
+        var configFile = path.join(configDir, 'getstream.js');
+
+        before(function() {
+            process.env.STREAM_NODE_CONFIG_DIR = configDir;
+
+            fs.mkdirSync(configDir);
+            fs.writeFileSync(configFile, 'exports.config = { apiKey: 12345 };');
+        });
+
+        after(function() { 
+            fs.unlink(configFile);
+            fs.rmdirSync(configDir);
+            delete process.env.STREAM_NODE_CONFIG_DIR;            
+        });
+
+        it('env var config dir', function() {
+            var settings = Config();
+
+            expect(settings.apiKey).to.be(12345);
+        });
+    });
+
+
+    it('override config', function() {
+
+    });
 });
