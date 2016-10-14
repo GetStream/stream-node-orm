942f67512256cd650456338ad1f81e9f6e4b855c
Matthisk Heimensen <m@tthisk.nl>
2016-10-14 11:36:59 +0200

    Run against a bunch of mongoose versions

--- src/backends/mongoose.js
+++ src/backends/mongoose.js
@@ -1,7 +1,8 @@
-var baseActivitySchemaPlugin = require('./activity.js');
-var util = require("util");
-var baseBackend = require('./base');
-var mongoose = require('mongoose');
+var baseActivitySchemaPlugin = require('./activity.js')
+  , util = require("util")
+  , stream = require('../index')
+  , baseBackend = require('./base')
+  , mongoose = require('mongoose');
 
 function Backend() {}
 
