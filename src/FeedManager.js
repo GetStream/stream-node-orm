dd5df2f57b42c2acd1cef97670075f27fe2136ca
Omar Adel <omarxadel21@gmail.com>
2021-12-07 20:49:19 +0200

    fix stream import

--- src/FeedManager.js
+++ src/FeedManager.js
@@ -1,109 +1,112 @@
-var stream = require('getstream').default,
-	Promise = require('promise');
-var FeedManager = function() {
-	this.initialize.apply(this, arguments);
+var stream = require("getstream"),
+  Promise = require("promise");
+var FeedManager = function () {
+  this.initialize.apply(this, arguments);
 };
 
 FeedManager.prototype = {
-	initialize: function(settings) {
-		this.settings = settings;
+  initialize: function (settings) {
+    this.settings = settings;
 
-		var options = {};
+    var options = {};
 
-		if (this.settings.apiLocation != '') {
-			options.location = this.settings.apiLocation;
-		}
+    if (this.settings.apiLocation != "") {
+      options.location = this.settings.apiLocation;
+    }
 
-		if (typeof process !== 'undefined' && process.env.STREAM_URL) {
-			this.client = stream.connect();
-		} else {
-			this.client = stream.connect(this.settings.apiKey, this.settings.apiSecret, this.settings.apiAppId, options);
-		}
-	},
+    if (typeof process !== "undefined" && process.env.STREAM_URL) {
+      this.client = stream.connect();
+    } else {
+      this.client = stream.connect(
+        this.settings.apiKey,
+        this.settings.apiSecret,
+        this.settings.apiAppId,
+        options
+      );
+    }
+  },
 
-	trackingEnabled: function(instance) {
-		return process.env.NODE_ENV === 'test' ? false : true;
-	},
+  trackingEnabled: function (instance) {
+    return process.env.NODE_ENV === "test" ? false : true;
+  },
 
-	getUserFeed: function(userId) {
-		return this.client.feed(this.settings.userFeed, userId);
-	},
+  getUserFeed: function (userId) {
+    return this.client.feed(this.settings.userFeed, userId);
+  },
 
-	getNotificationFeed: function(userId) {
-		return this.client.feed(this.settings.notificationFeed, userId);
-	},
+  getNotificationFeed: function (userId) {
+    return this.client.feed(this.settings.notificationFeed, userId);
+  },
 
-	getNewsFeeds: function(userId) {
-		var feeds = [];
-		var newsFeeds = this.settings.newsFeeds;
+  getNewsFeeds: function (userId) {
+    var feeds = [];
+    var newsFeeds = this.settings.newsFeeds;
 
-		for (var key in newsFeeds) {
-			var slug = newsFeeds[key];
-			feeds[slug] = this.client.feed(slug, userId);
-		}
+    for (var key in newsFeeds) {
+      var slug = newsFeeds[key];
+      feeds[slug] = this.client.feed(slug, userId);
+    }
 
-		return feeds;
-	},
+    return feeds;
+  },
 
-	followUser: function(userId, targetUserId) {
-		var newsFeeds = this.getNewsFeeds(userId);
-		var ps = [];
+  followUser: function (userId, targetUserId) {
+    var newsFeeds = this.getNewsFeeds(userId);
+    var ps = [];
 
-		for (var slug in newsFeeds) {
-			var p = newsFeeds[slug].follow(this.settings.userFeed, targetUserId);
-			ps.push(p);
-		}
+    for (var slug in newsFeeds) {
+      var p = newsFeeds[slug].follow(this.settings.userFeed, targetUserId);
+      ps.push(p);
+    }
 
-		return Promise.all(ps);
-	},
+    return Promise.all(ps);
+  },
 
-	unfollowUser: function(userId, targetUserId) {
-		var newsFeeds = this.getNewsFeeds(userId);
-		var ps = [];
+  unfollowUser: function (userId, targetUserId) {
+    var newsFeeds = this.getNewsFeeds(userId);
+    var ps = [];
 
-		for (var slug in newsFeeds) {
-			var p = newsFeeds[slug].unfollow(this.settings.userFeed, targetUserId);
-			ps.push(p);
-		}
+    for (var slug in newsFeeds) {
+      var p = newsFeeds[slug].unfollow(this.settings.userFeed, targetUserId);
+      ps.push(p);
+    }
 
-		return Promise.all(ps);
-	},
+    return Promise.all(ps);
+  },
 
-	getFeed: function(slug, userId) {
-		return this.client.feed(slug, userId);
-	},
+  getFeed: function (slug, userId) {
+    return this.client.feed(slug, userId);
+  },
 
-	activityCreated: function(instance) {
-		if (this.trackingEnabled(instance)) {
-			var activity = instance.createActivity();
-			var backend = instance.getStreamBackend();
+  activityCreated: function (instance) {
+    if (this.trackingEnabled(instance)) {
+      var activity = instance.createActivity();
+      var backend = instance.getStreamBackend();
 
-			backend.serializeActivities([activity]);
+      backend.serializeActivities([activity]);
 
-			var feedType =
-				instance.activityActorFeed() || this.settings.userFeed;
-			var userId = backend.getIdFromRef(activity.actor);
-			var feed = this.getFeed(feedType, userId);
+      var feedType = instance.activityActorFeed() || this.settings.userFeed;
+      var userId = backend.getIdFromRef(activity.actor);
+      var feed = this.getFeed(feedType, userId);
 
-			return feed.addActivity(activity);
-		}
-	},
+      return feed.addActivity(activity);
+    }
+  },
 
-	activityDeleted: function(instance) {
-		if (this.trackingEnabled(instance)) {
-			var activity = instance.createActivity();
-			var backend = instance.getStreamBackend();
+  activityDeleted: function (instance) {
+    if (this.trackingEnabled(instance)) {
+      var activity = instance.createActivity();
+      var backend = instance.getStreamBackend();
 
-			backend.serializeActivities([activity]);
+      backend.serializeActivities([activity]);
 
-			var feedType =
-				instance.activityActorFeed() || this.settings.userFeed;
-			var userId = backend.getIdFromRef(activity.actor);
-			var feed = this.getFeed(feedType, userId);
+      var feedType = instance.activityActorFeed() || this.settings.userFeed;
+      var userId = backend.getIdFromRef(activity.actor);
+      var feed = this.getFeed(feedType, userId);
 
-			return feed.removeActivity({ foreignId: activity.foreign_id });
-		}
-	},
+      return feed.removeActivity({ foreignId: activity.foreign_id });
+    }
+  },
 };
 
 module.exports = FeedManager;
